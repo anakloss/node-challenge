e3c4a893930a11794f482b641241162718f4abfd
Ana Kloss <anak492@gmail.com>
2022-08-27 15:43:26 -0300

    orden & DB

--- disney/app.js
+++ disney/app.js
@@ -3,16 +3,12 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var db = require('./config/database');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var characterRouter = require('./routes/characters');
-
-
-db.sync()
-  .then(() => console.log('BD conectada'));
-
+var movieRouter = require('./routes/movies');
+var genreRouter = require('./routes/genres');
 
 var app = express();
 
@@ -25,6 +21,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/characters', characterRouter);
+app.use('/movies', movieRouter);
+app.use('/genres', genreRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
