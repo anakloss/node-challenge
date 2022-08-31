a3dd262146c62e6957db2f97e1a639aafb5860c1
Ana Kloss <anak492@gmail.com>
2022-08-31 11:43:33 -0300

    delete

--- disney/routes/users.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const user = require('../controllers/user');
-
-router.get('/', user.list_all);
-router.get('/login', user.login);
-router.get('/register', user.register);
-
-module.exports = router;
