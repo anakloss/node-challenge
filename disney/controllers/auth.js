d556e5650da255370f9f3a5b18293c5efd12ce2c
Ana Kloss <anak492@gmail.com>
2022-08-31 23:47:56 -0300

    add send email

--- disney/controllers/auth.js
+++ disney/controllers/auth.js
@@ -1,5 +1,6 @@
 const User = require("../models/user");
 const jwt = require('jsonwebtoken');
+const mailer = require("../mailer/mailer");
 
 module.exports = {
   register: async function (req, res) {
@@ -7,11 +8,26 @@ module.exports = {
     const saveUser = await newUser.save();
     const token = jwt.sign({ id: saveUser.id }, process.env.JWT_SECRET, { expiresIn: 86400 });
 
-    res.status(200).json({ token });
+    // send email
+    if (token) { 
+      const email_destination = req.body.email;
+      const mailOptions = {
+        from: 'no-reply@disneyapi.com',
+        to: email_destination,
+        subject: 'Bienvenido a Disney API',
+        text: `Hola ${req.body.username},\n\n Bienvenido a Disney API de NodeJs Backend Challenge de Alkemy.`
+      };
+
+      mailer.sendMail(mailOptions, function (err) {
+        if (err) return res.status(401).json({ msg: err.message });
+      })
+    };
+
+    res.status(200).json({ token, msg: 'El mensaje de bienvenida fue enviado exitosamente' });
   },
+
   login: async function (req, res) {
     const userFound = await User.findOne({ where: { email: req.body.email } });
-
     if (!userFound) return res.status(400).json({ msg: 'User not found' });
 
     const matchPass = await userFound.comparePassword(req.body.password);
@@ -20,4 +36,5 @@ module.exports = {
     const token = jwt.sign({ id: userFound.id }, process.env.JWT_SECRET, { expiresIn: 86400 });
     res.json({ token });
   }
+
 }
---
+++ disney/mailer/mailer.js
@@ -0,0 +1,23 @@
+const nodemailer = require('nodemailer');
+const sgTransport = require('nodemailer-sendgrid-transport');
+
+let mailConfig;
+if (process.env.NODE_ENV === "production") {
+  const options = {
+    auth: {
+      api_key: process.env.SENDGRID_API_SECRET,
+    },
+  };
+  mailConfig = sgTransport(options);
+} else {
+  mailConfig = {
+    host: 'smtp.ethereal.email',
+    port: 587,
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASSWORD
+    },
+  };
+}
+
+module.exports = nodemailer.createTransport(mailConfig);
